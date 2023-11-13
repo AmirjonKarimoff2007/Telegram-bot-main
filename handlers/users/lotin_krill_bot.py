4f2ec78ef36f85f4da97b284e93d10a9f1085351
Amirjoin <amirjonkarimoff@gmail.com>
2023-11-13 10:05:32 +0500

    commit

--- handlers/users/lotin_krill_bot.py
+++ handlers/users/lotin_krill_bot.py
@@ -1,10 +1,2 @@
 from aiogram import types
 from loader import dp
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    # Lotin-Kirill tarjimoni yozing
-    translated_text = translate_function(message.text)
-
-    # Lotin-Kirill tarjimoni qaytarish
-    bot.reply_to(message, translated_text)
